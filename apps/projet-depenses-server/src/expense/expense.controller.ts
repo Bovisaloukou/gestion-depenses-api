99da180e1620bd2515a77289429f4b1f8d8a1456
Bovis ALOUKOU <bovisaloukou@gmail.com>
2025-10-24 12:36:22 -0700

    feat: finalisation des crud pour les entites

--- apps/projet-depenses-server/src/expense/expense.controller.ts
+++ apps/projet-depenses-server/src/expense/expense.controller.ts
@@ -3,6 +3,20 @@ import * as swagger from "@nestjs/swagger";
 import * as nestAccessControl from "nest-access-control";
 import { ExpenseService } from "./expense.service";
 import { ExpenseControllerBase } from "./base/expense.controller.base";
+import { Request } from "express";
+import { plainToClass } from "class-transformer";
+import { ExpenseFindManyArgs } from "./base/ExpenseFindManyArgs";
+import { UserInfo } from "../auth/UserInfo";
+import { UserData } from "../auth/userData.decorator";
+import { Expense } from "./base/Expense";
+import { ExpenseWhereUniqueInput } from "./base/ExpenseWhereUniqueInput";
+import { ExpenseCreateInput } from "./base/ExpenseCreateInput";
+import { ExpenseUpdateInput } from "./base/ExpenseUpdateInput";
+import * as errors from "../errors";
+import { AclFilterResponseInterceptor } from "../interceptors/aclFilterResponse.interceptor";
+import { AclValidateRequestInterceptor } from "../interceptors/aclValidateRequest.interceptor";
+import { isRecordNotFoundError } from "../prisma.util";
+import { ApiNestedQuery } from "../decorators/api-nested-query.decorator";
 
 @swagger.ApiTags("expenses")
 @common.Controller("expenses")
@@ -13,5 +27,251 @@ export class ExpenseController extends ExpenseControllerBase {
     protected readonly rolesBuilder: nestAccessControl.RolesBuilder
   ) {
     super(service, rolesBuilder);
+  }
+
+  @common.UseInterceptors(AclValidateRequestInterceptor)
+  @common.Post()
+  @swagger.ApiCreatedResponse({ type: Expense })
+  @nestAccessControl.UseRoles({
+    resource: "Expense",
+    action: "create",
+    possession: "any",
+  })
+  @swagger.ApiForbiddenResponse({
+    type: errors.ForbiddenException,
+  })
+  async createExpense(
+    @common.Body() data: ExpenseCreateInput,
+    @UserData() userInfo: UserInfo
+  ): Promise<Expense> {
+    return await this.service.createExpense({
+      data: {
+        ...data,
+        category: data.category
+          ? {
+              connect: data.category,
+            }
+          : undefined,
+        user: {
+          connect: {
+            id: userInfo.id,
+          },
+        },
+      },
+      select: {
+        amount: true,
+        category: {
+          select: {
+            id: true,
+          },
+        },
+        createdAt: true,
+        date: true,
+        description: true,
+        id: true,
+        updatedAt: true,
+        user: {
+          select: {
+            id: true,
+          },
+        },
+      },
+    });
+  }
+
+  @common.UseInterceptors(AclFilterResponseInterceptor)
+  @common.Get()
+  @swagger.ApiOkResponse({ type: [Expense] })
+  @ApiNestedQuery(ExpenseFindManyArgs)
+  @nestAccessControl.UseRoles({
+    resource: "Expense",
+    action: "read",
+    possession: "any",
+  })
+  @swagger.ApiForbiddenResponse({
+    type: errors.ForbiddenException,
+  })
+  async expenses(
+    @common.Req() request: Request,
+    @UserData() userInfo: UserInfo
+  ): Promise<Expense[]> {
+    const args = plainToClass(ExpenseFindManyArgs, request.query);
+    return this.service.expenses({
+      ...args,
+      where: {
+        ...args.where,
+        user: {
+          id: userInfo.id,
+        },
+      },
+      select: {
+        amount: true,
+        category: {
+          select: {
+            id: true,
+          },
+        },
+        createdAt: true,
+        date: true,
+        description: true,
+        id: true,
+        updatedAt: true,
+        user: {
+          select: {
+            id: true,
+          },
+        },
+      },
+    });
+  }
+
+  @common.UseInterceptors(AclFilterResponseInterceptor)
+  @common.Get("/:id")
+  @swagger.ApiOkResponse({ type: Expense })
+  @swagger.ApiNotFoundResponse({ type: errors.NotFoundException })
+  @nestAccessControl.UseRoles({
+    resource: "Expense",
+    action: "read",
+    possession: "own",
+  })
+  @swagger.ApiForbiddenResponse({
+    type: errors.ForbiddenException,
+  })
+  async expense(
+    @common.Param() params: ExpenseWhereUniqueInput,
+    @UserData() userInfo: UserInfo
+  ): Promise<Expense | null> {
+    const result = await this.service.expense({
+      where: { id: params.id, user: { id: userInfo.id } },
+      select: {
+        amount: true,
+        category: {
+          select: {
+            id: true,
+          },
+        },
+        createdAt: true,
+        date: true,
+        description: true,
+        id: true,
+        updatedAt: true,
+        user: {
+          select: {
+            id: true,
+          },
+        },
+      },
+    });
+    if (result === null) {
+      throw new errors.NotFoundException(
+        `No resource was found for ${JSON.stringify(params)}`
+      );
+    }
+    return result;
+  }
+
+  @common.UseInterceptors(AclValidateRequestInterceptor)
+  @common.Patch("/:id")
+  @swagger.ApiOkResponse({ type: Expense })
+  @swagger.ApiNotFoundResponse({ type: errors.NotFoundException })
+  @nestAccessControl.UseRoles({
+    resource: "Expense",
+    action: "update",
+    possession: "any",
+  })
+  @swagger.ApiForbiddenResponse({
+    type: errors.ForbiddenException,
+  })
+  async updateExpense(
+    @common.Param() params: ExpenseWhereUniqueInput,
+    @common.Body() data: ExpenseUpdateInput,
+    @UserData() userInfo: UserInfo
+  ): Promise<Expense | null> {
+    try {
+      return await this.service.updateExpense({
+        where: { id: params.id, user: { id: userInfo.id } },
+        data: {
+          ...data,
+          user: undefined,
+          category: data.category
+            ? {
+                connect: data.category,
+              }
+            : undefined,
+        },
+        select: {
+          amount: true,
+          category: {
+            select: {
+              id: true,
+            },
+          },
+          createdAt: true,
+          date: true,
+          description: true,
+          id: true,
+          updatedAt: true,
+          user: {
+            select: {
+              id: true,
+            },
+          },
+        },
+      });
+    } catch (error) {
+      if (isRecordNotFoundError(error)) {
+        throw new errors.NotFoundException(
+          `No resource was found for ${JSON.stringify(params)}`
+        );
+      }
+      throw error;
+    }
+  }
+
+  @common.Delete("/:id")
+  @swagger.ApiOkResponse({ type: Expense })
+  @swagger.ApiNotFoundResponse({ type: errors.NotFoundException })
+  @nestAccessControl.UseRoles({
+    resource: "Expense",
+    action: "delete",
+    possession: "any",
+  })
+  @swagger.ApiForbiddenResponse({
+    type: errors.ForbiddenException,
+  })
+  async deleteExpense(
+    @common.Param() params: ExpenseWhereUniqueInput,
+    @UserData() userInfo: UserInfo
+  ): Promise<Expense | null> {
+    try {
+      return await this.service.deleteExpense({
+        where: { id: params.id, user: { id: userInfo.id } },
+        select: {
+          amount: true,
+          category: {
+            select: {
+              id: true,
+            },
+          },
+          createdAt: true,
+          date: true,
+          description: true,
+          id: true,
+          updatedAt: true,
+          user: {
+            select: {
+              id: true,
+            },
+          },
+        },
+      });
+    } catch (error) {
+      if (isRecordNotFoundError(error)) {
+        throw new errors.NotFoundException(
+          `No resource was found for ${JSON.stringify(params)}`
+        );
+      }
+      throw error;
+    }
   }
 }
--- apps/projet-depenses-server/src/trackedCrypto/trackedCrypto.controller.ts
+++ apps/projet-depenses-server/src/trackedCrypto/trackedCrypto.controller.ts
@@ -3,6 +3,20 @@ import * as swagger from "@nestjs/swagger";
 import * as nestAccessControl from "nest-access-control";
 import { TrackedCryptoService } from "./trackedCrypto.service";
 import { TrackedCryptoControllerBase } from "./base/trackedCrypto.controller.base";
+import { Request } from "express";
+import { plainToClass } from "class-transformer";
+import { TrackedCryptoFindManyArgs } from "./base/TrackedCryptoFindManyArgs";
+import { UserInfo } from "../auth/UserInfo";
+import { UserData } from "../auth/userData.decorator";
+import { TrackedCrypto } from "./base/TrackedCrypto";
+import { TrackedCryptoWhereUniqueInput } from "./base/TrackedCryptoWhereUniqueInput";
+import { TrackedCryptoCreateInput } from "./base/TrackedCryptoCreateInput";
+import { TrackedCryptoUpdateInput } from "./base/TrackedCryptoUpdateInput";
+import * as errors from "../errors";
+import { AclFilterResponseInterceptor } from "../interceptors/aclFilterResponse.interceptor";
+import { AclValidateRequestInterceptor } from "../interceptors/aclValidateRequest.interceptor";
+import { isRecordNotFoundError } from "../prisma.util";
+import { ApiNestedQuery } from "../decorators/api-nested-query.decorator";
 
 @swagger.ApiTags("trackedCryptos")
 @common.Controller("trackedCryptos")
@@ -13,5 +27,206 @@ export class TrackedCryptoController extends TrackedCryptoControllerBase {
     protected readonly rolesBuilder: nestAccessControl.RolesBuilder
   ) {
     super(service, rolesBuilder);
+  }
+
+  @common.UseInterceptors(AclValidateRequestInterceptor)
+  @common.Post()
+  @swagger.ApiCreatedResponse({ type: TrackedCrypto })
+  @nestAccessControl.UseRoles({
+    resource: "TrackedCrypto",
+    action: "create",
+    possession: "any",
+  })
+  @swagger.ApiForbiddenResponse({
+    type: errors.ForbiddenException,
+  })
+  async createTrackedCrypto(
+    @common.Body() data: TrackedCryptoCreateInput,
+    @UserData() userInfo: UserInfo
+  ): Promise<TrackedCrypto> {
+    return await this.service.createTrackedCrypto({
+      data: {
+        ...data,
+        user: {
+          connect: {
+            id: userInfo.id,
+          },
+        },
+      },
+      select: {
+        createdAt: true,
+        id: true,
+        tokenId: true,
+        updatedAt: true,
+        user: {
+          select: {
+            id: true,
+          },
+        },
+      },
+    });
+  }
+
+  @common.UseInterceptors(AclFilterResponseInterceptor)
+  @common.Get()
+  @swagger.ApiOkResponse({ type: [TrackedCrypto] })
+  @ApiNestedQuery(TrackedCryptoFindManyArgs)
+  @nestAccessControl.UseRoles({
+    resource: "TrackedCrypto",
+    action: "read",
+    possession: "any",
+  })
+  @swagger.ApiForbiddenResponse({
+    type: errors.ForbiddenException,
+  })
+  async trackedCryptos(
+    @common.Req() request: Request,
+    @UserData() userInfo: UserInfo
+  ): Promise<TrackedCrypto[]> {
+    const args = plainToClass(TrackedCryptoFindManyArgs, request.query);
+    return this.service.trackedCryptos({
+      ...args,
+      where: {
+        ...args.where,
+        user: {
+          id: userInfo.id,
+        },
+      },
+      select: {
+        createdAt: true,
+        id: true,
+        tokenId: true,
+        updatedAt: true,
+        user: {
+          select: {
+            id: true,
+          },
+        },
+      },
+    });
+  }
+
+  @common.UseInterceptors(AclFilterResponseInterceptor)
+  @common.Get("/:id")
+  @swagger.ApiOkResponse({ type: TrackedCrypto })
+  @swagger.ApiNotFoundResponse({ type: errors.NotFoundException })
+  @nestAccessControl.UseRoles({
+    resource: "TrackedCrypto",
+    action: "read",
+    possession: "own",
+  })
+  @swagger.ApiForbiddenResponse({
+    type: errors.ForbiddenException,
+  })
+  async trackedCrypto(
+    @common.Param() params: TrackedCryptoWhereUniqueInput,
+    @UserData() userInfo: UserInfo
+  ): Promise<TrackedCrypto | null> {
+    const result = await this.service.trackedCrypto({
+      where: { id: params.id, user: { id: userInfo.id } },
+      select: {
+        createdAt: true,
+        id: true,
+        tokenId: true,
+        updatedAt: true,
+        user: {
+          select: {
+            id: true,
+          },
+        },
+      },
+    });
+    if (result === null) {
+      throw new errors.NotFoundException(
+        `No resource was found for ${JSON.stringify(params)}`
+      );
+    }
+    return result;
+  }
+
+  @common.UseInterceptors(AclValidateRequestInterceptor)
+  @common.Patch("/:id")
+  @swagger.ApiOkResponse({ type: TrackedCrypto })
+  @swagger.ApiNotFoundResponse({ type: errors.NotFoundException })
+  @nestAccessControl.UseRoles({
+    resource: "TrackedCrypto",
+    action: "update",
+    possession: "any",
+  })
+  @swagger.ApiForbiddenResponse({
+    type: errors.ForbiddenException,
+  })
+  async updateTrackedCrypto(
+    @common.Param() params: TrackedCryptoWhereUniqueInput,
+    @common.Body() data: TrackedCryptoUpdateInput,
+    @UserData() userInfo: UserInfo
+  ): Promise<TrackedCrypto | null> {
+    try {
+      return await this.service.updateTrackedCrypto({
+        where: { id: params.id, user: { id: userInfo.id } },
+        data: {
+          ...data,
+          user: undefined,
+        },
+        select: {
+          createdAt: true,
+          id: true,
+          tokenId: true,
+          updatedAt: true,
+          user: {
+            select: {
+              id: true,
+            },
+          },
+        },
+      });
+    } catch (error) {
+      if (isRecordNotFoundError(error)) {
+        throw new errors.NotFoundException(
+          `No resource was found for ${JSON.stringify(params)}`
+        );
+      }
+      throw error;
+    }
+  }
+
+  @common.Delete("/:id")
+  @swagger.ApiOkResponse({ type: TrackedCrypto })
+  @swagger.ApiNotFoundResponse({ type: errors.NotFoundException })
+  @nestAccessControl.UseRoles({
+    resource: "TrackedCrypto",
+    action: "delete",
+    possession: "any",
+  })
+  @swagger.ApiForbiddenResponse({
+    type: errors.ForbiddenException,
+  })
+  async deleteTrackedCrypto(
+    @common.Param() params: TrackedCryptoWhereUniqueInput,
+    @UserData() userInfo: UserInfo
+  ): Promise<TrackedCrypto | null> {
+    try {
+      return await this.service.deleteTrackedCrypto({
+        where: { id: params.id, user: { id: userInfo.id } },
+        select: {
+          createdAt: true,
+          id: true,
+          tokenId: true,
+          updatedAt: true,
+          user: {
+            select: {
+              id: true,
+            },
+          },
+        },
+      });
+    } catch (error) {
+      if (isRecordNotFoundError(error)) {
+        throw new errors.NotFoundException(
+          `No resource was found for ${JSON.stringify(params)}`
+        );
+      }
+      throw error;
+    }
   }
 }
